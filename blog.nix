9152881abb4f8ecfc1735fa8b2c619df961cedff
Robin Mattheussen <me@romatthe.dev>
2021-04-01 06:55:25 +0200

    Basics structure and nix build done

---
+++ blog.nix
@@ -0,0 +1,14 @@
+{ pkgs, stdenv, ... }:
+
+stdenv.mkDerivation rec {
+  pname = "romatthe-blog";
+  version = "0.0.1";
+  src = ./.;
+  buildInputs = [ pkgs.zola ];
+  buildPhase = ''
+    zola build
+  '';
+  installPhase = ''
+    cp -r public $out
+  '';
+}
--- flake.nix
+++ flake.nix
@@ -3,14 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url   = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in {
-      devShell.x86_64-linux = pkgs.mkShell {
-        buildInputs = [ pkgs.zola ];
+      pkgsFor = system: import nixpkgs {
+        inherit system;
+        overlays = [ self.overlay ];
       };
-    };
+    in {
+      overlay = final: prev: {
+        blog = prev.callPackage ./blog.nix {};
+      };
+    } // flake-utils.lib.eachDefaultSystem (system: 
+      let pkgs = pkgsFor system;
+      in
+        {
+          defaultPackage = pkgs.blog;
+          
+          devShell = pkgs.mkShell {
+            buildInputs = [ pkgs.zola ];
+          };
+        });
 }
